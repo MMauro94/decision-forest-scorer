94f8e065c80c39586823a29b7657e78e76681476
Mauro Molin <molin.mauro@femastudios.com>
2019-10-28 17:30:11 +0100

    Removed debug statements

--- FeaturesMap.h
+++ FeaturesMap.h
@@ -12,7 +12,6 @@
 
 class FeatureEvaluator {
 private:
-    std::vector<std::shared_ptr<InternalNode>> nodes;
     std::vector<double> splittingThreshold;
     std::vector<Epitome> masks;
     std::vector<int> treeIndexed;
@@ -20,19 +19,18 @@ public:
     FeatureEvaluator(
             Forest &forest,
             int featureIndex
-    ) : nodes(forest.getNodesForFeature(featureIndex)) {
-        std::sort(this->nodes.begin(), this->nodes.end(), [](auto &a, auto &b) -> bool {
+    ) {
+        auto nodes = forest.getNodesForFeature(featureIndex);
+        std::sort(nodes.begin(), nodes.end(), [](auto &a, auto &b) -> bool {
             return a->splittingThreshold < b->splittingThreshold;
         });
 
 
-        for (auto &n : this->nodes) {
-            printf("Found node %f for feature %d\n", n->splittingThreshold, featureIndex);
+        for (auto &n : nodes) {
             splittingThreshold.push_back(n->splittingThreshold);
             treeIndexed.push_back(n->getTreeIndex());
 
-            auto ep = masks.emplace_back(forest.trees[n->getTreeIndex()].countLeafsUntil(n), n->leftNode->numberOfLeafs());
-            std::cout << ep << std::endl;
+            masks.emplace_back(forest.trees[n->getTreeIndex()].countLeafsUntil(n), n->leftNode->numberOfLeafs());
         }
     }
 };
