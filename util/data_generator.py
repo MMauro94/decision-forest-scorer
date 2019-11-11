644546f1bb5b931077bc407db81a25c6a630438b
Francesco Benvenuto <francesco_benvenuto@me.com>
2019-11-11 17:47:45 +0100

    add data generator

---
+++ util/data_generator.py
@@ -0,0 +1,59 @@
+import os
+import json
+
+import numpy as np
+import lightgbm
+from sklearn.datasets import load_svmlight_file
+import itertools
+import sys
+import re
+
+fold_path = sys.argv[1]
+
+if fold_path is None:
+    sys.exit(1)
+
+test_file = f"{fold_path}/test.txt"
+train_file = f"{fold_path}/train.txt"
+
+test_data = load_svmlight_file(test_file, query_id=True)
+train_data = load_svmlight_file(train_file, query_id=True)
+
+query_lens = [
+    sum(1 for _ in group) for key, group in itertools.groupby(train_data[2])
+]
+train_lgb = lightgbm.Dataset(
+    data=train_data[0], label=train_data[1], group=query_lens
+)
+
+params = {
+    "objective": "lambdarank",  # what to optimize during training
+    "max_position": 10,  # threshold used in optimizing lamdarank (NDCG)
+    "learning_rate": 0.1,
+    "num_leaves": 4,
+    "min_data_in_leaf": 5,
+    "metric": ["ndcg"],  # what to use/print for evaluation
+    "ndcg_eval_at": 10,
+}
+
+lgbm_info = {}
+
+lgbm_model = lightgbm.train(
+    params,
+    train_lgb,  # training dataser
+    num_boost_round=10,  # num trees
+    valid_sets=[train_lgb],
+    valid_names=["train"],
+    evals_result=lgbm_info,
+    verbose_eval=True,
+)
+
+print("model created")
+
+with open(f"{fold_path}/test_scores.txt", "wb") as f:
+    lines = [(str(x) + "\n").encode() for x in lgbm_model.predict(test_data[0])]
+    f.writelines(lines)
+
+
+with open(f"{fold_path}/model.json", "wb") as f:
+    f.write(json.dumps(lgbm_model.dump_model()).encode())
